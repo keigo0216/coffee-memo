3039ac32be22c42cf55c0d542c01691961b7db79
saitosaikyo <keigo-5787-tarou@ezweb.ne.jp>
2023-07-11 04:25:58 +0000

    Added image display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
     </head>
     <body>
         <h1>Home</h1>
-        <p>Hello, {{ $name }}!</p>
+        <p>coffee_name {{ $coffee_data['coffee_name'] }}</p>
+        <p>evaluation {{ $coffee_data['evaluation'] }}</p>
+        <p>date {{ $coffee_data['date'] }}</p>
+        <img src="{{ asset($coffee_data['img']) }}" alt="coffee_image">
     </body>
 </html>
