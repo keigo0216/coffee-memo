30783ea197498bb168a91a3b56314b1a85a7643a
Keigo Nakano <keigo-5787-tarou@ezweb.ne.jp>
2023-07-13 11:46:56 +0000

    creating Coffee model and relation between Coffee and User model

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,9 +4,8 @@
     </head>
     <body>
         <h1>list</h1>
-        <p>coffee_name {{ $coffee_data['coffee_name'] }}</p>
-        <p>evaluation {{ $coffee_data['evaluation'] }}</p>
-        <p>date {{ $coffee_data['date'] }}</p>
-        <img src="{{ asset($coffee_data['img']) }}" alt="coffee_image">
+        @foreach ($coffees as $coffee)
+        <p>{{ $coffee->name }}</p>
+        @endforeach
     </body>
 </html>
