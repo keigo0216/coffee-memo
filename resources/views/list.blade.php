facb7f0f375457846f77e1cd25bdf3f26582bbc9
Keigo Nakano <keigo-5787-tarou@ezweb.ne.jp>
2023-07-13 13:09:00 +0000

    add evaluation display

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,6 +6,7 @@
         <h1>list</h1>
         @foreach ($coffees as $coffee)
         <p>{{ $coffee->name }}</p>
+        <p>評価値:{{ $coffee->evaluation}}</p>
         <img src="{{ $coffee->img }}" alt="coffee_image">
         @endforeach
     </body>
