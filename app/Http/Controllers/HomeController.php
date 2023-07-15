3fce6d798c1e3e6902d51c87bcc336434165caf6
Keigo Nakano <keigo-5787-tarou@ezweb.ne.jp>
2023-07-15 00:40:22 +0000

    Adding Image resizing function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Coffee;
+use InterventionImage;
 
 class HomeController extends Controller
 {
@@ -52,17 +53,23 @@ class HomeController extends Controller
     {
         $coffee = new Coffee;
 
-        // 画像を保存
+        // 画像を保存先パス
         $dir = 'coffee_image';
-        $img_path = $request->file('coffee_img')->getClientOriginalName();
-        $request->file('coffee_img')->storeAs('public/' . $dir, $img_path);
+        // formで送られてきた画像を変数に格納
+        $coffee_img = $request->file('coffee_img');
+        // 画像の名前を取得
+        $img_name = $coffee_img->getClientOriginalName();
+        // アスペクト比を維持したまま、画像のサイズを変更
+        $img = InterventionImage::make($coffee_img)->resize(300, null, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save(storage_path() . '/app/public/' . $dir . '/' . $img_name);
 
         $form = $request->all();
         $form['user_id'] = Auth::id();
         unset($form['_token']);
         
         // 画像のパスをDBに保存
-        $form['img'] = 'storage/' . $dir . '/' . $img_path;
+        $form['img'] = 'storage/' . $dir . '/' . $img_name;
 
         $coffee->fill($form)->save();
         return redirect('/list');
