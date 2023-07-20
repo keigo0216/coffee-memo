105dd461d52cfa984aadefbfe257343c9ec1601d
Keigo Nakano <keigo-5787-tarou@ezweb.ne.jp>
2023-07-20 02:54:44 +0000

    Setted list page to / link and made logout action

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get(
-    '/list',
+    '/',
     [App\Http\Controllers\HomeController::class, 'list']
 )->name('list');
 
